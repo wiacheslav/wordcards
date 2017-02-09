bb695fff76b72eabdee97d6706ec5e74cc4229f3
Vyacheslav Yushchenko <scrynkaa@gmail.com>
2017-02-10 00:04:22 +0200

    add words

--- src/app/word-card.ts
+++ src/app/word-card.ts
@@ -64,5 +64,105 @@ export const CARDS: WordCard[] = [
         id: 11,
         english: `shareholder`,
         russian: `акционер, держатель акций`
+    },
+    {
+        id: 12,
+        english: `fleet of truks`,
+        russian: `парк грузовых автомобилей`
+    },
+    {
+        id: 11,
+        english: `flavour`,
+        russian: `вкус`
+    },
+    {
+        id: 11,
+        english: `expand rapidly`,
+        russian: `быстро расширяться`
+    },
+    {
+        id: 11,
+        english: `environment`,
+        russian: `окружение, окружающая среда`
+    },
+    {
+        id: 11,
+        english: `expand`,
+        russian: `растягиваться, расширяться; увеличиваться в объёме, в размерах, в количестве`
+    },
+    {
+        id: 11,
+        english: `impatient`,
+        russian: `нетерпеливый, нетерпимый`
+    },
+    {
+        id: 11,
+        english: `contribution`,
+        russian: `вклад, взнос, пожертвование`
+    },
+    {
+        id: 11,
+        english: `employee`,
+        russian: `работник`
+    },
+    {
+        id: 11,
+        english: `dedicated`,
+        russian: `преданный`
+    },
+    {
+        id: 11,
+        english: `indulgence`,
+        russian: `снисходительность, терпимость`
+    },
+    {
+        id: 11,
+        english: `cut the price`,
+        russian: `снизить цену`
+    },
+    {
+        id: 11,
+        english: `consumer`,
+        russian: `потребитель`
+    },
+    {
+        id: 11,
+        english: `competitive`,
+        russian: `конкурентоспособный`
+    },
+    {
+        id: 11,
+        english: `cashflow`,
+        russian: `поток наличности`
+    },
+    {
+        id: 11,
+        english: `meeting in person`,
+        russian: `личная встреча`
+    },
+    {
+        id: 11,
+        english: `board meeting`,
+        russian: `заседание правления`
+    },
+    {
+        id: 11,
+        english: `all time high`,
+        russian: `небывало-высокий уровень`
+    },
+    {
+        id: 11,
+        english: `cooling-off period`,
+        russian: `период обдумывания`
+    },
+    {
+        id: 11,
+        english: `convenience store`,
+        russian: `вечерний магазин, ночной магазин (небольшой магазин, торгующий самыми необходимыми товарами, в основном продуктами, и работающий дольше магазинов, иногда круглосуточно)`
+    },
+    {
+        id: 11,
+        english: `lead`,
+        russian: `потенциальный покупатель`
     }
 ];
