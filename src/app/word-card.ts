125d25302dac2be2cf2bb838fa34b9440ed6b980
Vyacheslav Yushchenko <scrynkaa@gmail.com>
2017-02-10 20:18:25 +0200

    full dictioanary

--- src/app/word-card.ts
+++ src/app/word-card.ts
@@ -164,5 +164,295 @@ export const CARDS: WordCard[] = [
         id: 11,
         english: `lead`,
         russian: `потенциальный покупатель`
+    },
+    {
+        id: 1,
+        english: `share price`,
+        russian: `биржевый курс, курс акций`
+    },
+    {
+        id: 2,
+        english: `money-back guarantee`,
+        russian: `гарантия возврата денег`
+    },
+    {
+        id: 3,
+        english: `asset`,
+        russian: `активый, фонды, ресурсы`
+    },
+    {
+        id: 4,
+        english: `bargain`,
+        russian: `выгодная покупка; дешёво купленная вещь`
+    },
+    {
+        id: 5,
+        english: `additive`,
+        russian: `добавка (пищевая)`
+    },
+    {
+        id: 6,
+        english: `market share`,
+        russian: `доля на рынке`
+    },
+    {
+        id: 7,
+        english: `mindset`,
+        russian: `склад ума`
+    },
+    {
+        id: 8,
+        english: `We are planning...`,
+        russian: `...`
+    },
+    {
+        id: 9,
+        english: `Finally, thanks to...`,
+        russian: `...`
+    },
+    {
+        id: 10,
+        english: `We've increased / decreased`,
+        russian: `...`
+    },
+    {
+        id: 11,
+        english: `to dispatch`,
+        russian: `отослать, отправлять по назначению, отгружать (товары)`
+    },
+    {
+        id: 12,
+        english: `to buy in bulk`,
+        russian: `покупать оптом`
+    },
+    {
+        id: 11,
+        english: `suitable`,
+        russian: `подходящий`
+    },
+    {
+        id: 11,
+        english: `setback`,
+        russian: `неудача`
+    },
+    {
+        id: 11,
+        english: `storage`,
+        russian: `хранилище, кладовка`
+    },
+    {
+        id: 11,
+        english: `health conscious`,
+        russian: `сознательно относящийся к здоровью`
+    },
+    {
+        id: 11,
+        english: `obvious`,
+        russian: `очевидный, ясный`
+    },
+    {
+        id: 11,
+        english: `net profit`,
+        russian: `чистая прибыль`
+    },
+    {
+        id: 11,
+        english: `head office`,
+        russian: `главный офис`
+    },
+    {
+        id: 11,
+        english: `fudge`,
+        russian: `ирис с добавлением молока`
+    },
+    {
+        id: 11,
+        english: `slot`,
+        russian: `позиция`
+    },
+    {
+        id: 11,
+        english: `subsidiary`,
+        russian: `дочерняя компания, филиал`
+    },
+    {
+        id: 11,
+        english: `wholesaler`,
+        russian: `оптовый торговец`
+    },
+    {
+        id: 11,
+        english: `workforce`,
+        russian: `рабочая сила, работники`
+    },
+    {
+        id: 11,
+        english: `head office / headquarter`,
+        russian: `главный офис`
+    },
+    {
+        id: 11,
+        english: `Increased / decreased production and strong / weak demand have`,
+        russian: `...`
+    },
+    {
+        id: 11,
+        english: `We have had excelent / average / poor performance`,
+        russian: `...`
+    },
+    {
+        id: 11,
+        english: `sustainability`,
+        russian: `устойчивость, выживаемость`
+    },
+    {
+        id: 11,
+        english: `subsidiary`,
+        russian: `филиал`
+    },
+    {
+        id: 11,
+        english: `storage facility`,
+        russian: `склад; складское сооружение`
+    },
+    {
+        id: 11,
+        english: `turnover`,
+        russian: `оборот`
+    },
+    {
+        id: 1,
+        english: `share price`,
+        russian: `цена акции, цена на бирже`
+    },
+    {
+        id: 2,
+        english: `to know inside out`,
+        russian: `знать досконально`
+    },
+    {
+        id: 3,
+        english: `set aside`,
+        russian: `откладывать на время; прерывать, приберегать (деньги); оставлять (время)`
+    },
+    {
+        id: 4,
+        english: `contribute`,
+        russian: `содействовать, способствовать, делать (вносить) вклад`
+    },
+    {
+        id: 5,
+        english: `run`,
+        russian: `руководить`
+    },
+    {
+        id: 6,
+        english: `competitor`,
+        russian: `конкурент, противник, соперник`
+    },
+    {
+        id: 7,
+        english: `warehouse`,
+        russian: `товарный склад`
+    },
+    {
+        id: 8,
+        english: `turnover`,
+        russian: `товарооборот`
+    },
+    {
+        id: 9,
+        english: `country manager`,
+        russian: `региональный менеджер`
+    },
+    {
+        id: 10,
+        english: `seasoned seller`,
+        russian: `опытный продавец`
+    },
+    {
+        id: 11,
+        english: `parent-company`,
+        russian: `компания-учредитель`
+    },
+    {
+        id: 12,
+        english: `offer a solution`,
+        russian: `предложить решение`
+    },
+    {
+        id: 11,
+        english: `performance`,
+        russian: `выполнение, исполнение`
+    },
+    {
+        id: 11,
+        english: `product capacity`,
+        russian: `объём продукции`
+    },
+    {
+        id: 11,
+        english: `community`,
+        russian: `объединение, содружество, сообщество`
+    },
+    {
+        id: 11,
+        english: `criteria`,
+        russian: `критерий`
+    },
+    {
+        id: 11,
+        english: `pre-tax profit`,
+        russian: `прибыль до налогообложения`
+    },
+    {
+        id: 11,
+        english: `remarkable`,
+        russian: `замечательный, выдающийся`
+    },
+    {
+        id: 11,
+        english: `customer needs`,
+        russian: `нужды покупателей`
+    },
+    {
+        id: 11,
+        english: `incentive`,
+        russian: `стимул`
+    },
+    {
+        id: 11,
+        english: `user-friendly packaging`,
+        russian: `практичная упаковка`
+    },
+    {
+        id: 11,
+        english: `revenue`,
+        russian: `доход`
+    },
+    {
+        id: 11,
+        english: `stay ahead`,
+        russian: `работать на опережение`
+    },
+    {
+        id: 11,
+        english: `cash flow`,
+        russian: `поток наличности, движение наличных`
+    },
+    {
+        id: 11,
+        english: `sponsorship`,
+        russian: `спонсорство`
+    },
+    {
+        id: 11,
+        english: `after-sales service`,
+        russian: `гарантийное обслуживание; послепродажное обслуживание; сервисное обслуживание`
+    },
+    {
+        id: 11,
+        english: `retire`,
+        russian: `уходить в отставку, на пенсию`
     }
 ];
